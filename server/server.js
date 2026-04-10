3db36da088c79023d04d8f708229ab69580add70
Shashikumar Mulimani <shree.mata.hbl@gmail.com>
2026-04-10 14:59:01 +0530

    first commit17

--- server/server.js
+++ server/server.js
@@ -37,6 +37,25 @@ app.post('/upload', upload.single('pdf'), async (req, res) => {
     }
 });
 
+
+app.post('/print', async (req, res) => {
+    try {
+        const { fileUrl, duplex } = req.body;
+
+        console.log("Printing file:", fileUrl);
+        console.log("Duplex:", duplex);
+
+        // TODO: Add actual printing logic here
+        // Example: send to printer / download file / etc.
+
+        res.send("Print started successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Print failed");
+    }
+});
+
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(PORT, () => console.log("Server running"));
